04aba55609d3365976255af96bef491057d62bde
Alex Ku <alex.ku96@outlook.com>
2019-02-20 11:46:56 -0500

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string TimeOfDay;
+
+            if (hour < 12)
+            {
+                TimeOfDay = "Good Morning";
+            }
+            else if (hour < 18)
+            {
+                TimeOfDay = "Good Afternoon";
+            }
+            else
+            {
+                TimeOfDay = "Good Evening";
+            }
+
+            ViewBag.Greeting = TimeOfDay;
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
